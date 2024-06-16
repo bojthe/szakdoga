90801c37e236e4e4c2a1499387a6c18944adf39a
bojth <bojthe_robert@yahoo.com>
2024-06-16 13:48:40 +0200

    Implemented separate functions for video processing.

--- videoProcessing.py
+++ videoProcessing.py
@@ -1,7 +1,14 @@
 import cv2
 from tello_asyncio import VIDEO_URL
 
+
 class videoProcessor:
+    # [red, orange, yellow, light-green, green]
+    COLOR_STAGES = [(0, 0, 255), (0, 128, 255), (0, 255, 255), (0, 255, 128), (0, 255, 0)]
+    # the size of the QR code edge in pixels from that distance
+    SIDE_SIZES = {"30": 202, "40": 116, "50": 90}
+    # the margin of error in pixels from that distance
+    MARGINS = {"30": 20, "40": 12, "50": 9}
 
     def __init__(self, angle, distance, vertical, horizontal, clockwise,
                  counterclockwise, left, right, up, down, forward, backward,
@@ -39,3 +46,145 @@ class videoProcessor:
             if self.capture:
                 self.capture.release()
             cv2.destroyAllWindows()
+
+    def __interpretQR(self, frame, points, horizontalPosition, verticalPosition, distance):
+        if not self.angleOkEvent.is_set():
+            self.__controlAngle(points, distance)
+            self.__box(frame, points, 0)
+        elif not self.distanceOkEvent.is_set():
+            self.__controlDistance(points, distance)
+            self.__box(frame, points, 1)
+        elif not self.verticalOkEvent.is_set():
+            self.__controlVerticalPosition(frame, points, distance, verticalPosition)
+            self.__box(frame, points, 2)
+        elif not self.horizontalOkEvent.is_set():
+            self.__controlHorizontalPosition(frame, points, distance, horizontalPosition)
+            self.__box(frame, points, 3)
+        else:
+            self.__box(frame, points, 4)
+            self.__clearMovementEvents()
+
+
+    def __clearMovementEvents(self):
+        self.turnClockwiseEvent.clear()
+        self.turnCounterClockwiseEvent.clear()
+        self.moveLeftEvent.clear()
+        self.moveRightEvent.clear()
+        self.moveUpEvent.clear()
+        self.moveDownEvent.clear()
+        self.moveForwardEvent.clear()
+        self.moveBackwardEvent.clear()
+
+
+    def __box(self, frame, points, colorIndex):
+        for i in range(len(points)):
+            pt1 = [int(val) for val in points[i]]
+            pt2 = [int(val) for val in points[(i + 1) % 4]]
+            # print('i:' + str(i) + ' pt1: ' + str(pt1) + ' pt2: ' + str(pt2))
+            cv2.line(frame, pt1, pt2, color=self.COLOR_STAGES[colorIndex], thickness=2)
+
+
+    def __controlAngle(self, points, distance):
+        # if left side is smaller than right side ---> turn clockwise
+        if points[3][1]-points[0][1]-points[2][1]-points[1][1] < 0-self.MARGINS[distance]:
+            self.turnCounterClockwiseEvent.clear()
+            self.turnClockwiseEvent.set()
+        # if right side smaller than left side ---> turn counter clockwise
+        elif points[3][1]-points[0][1]-points[2][1]-points[1][1] > self.MARGINS[distance]:
+            self.turnClockwiseEvent.clear()
+            self.turnCounterClockwiseEvent.set()
+        # if both sides within margin of error ---> angle is correct
+        else:
+            self.turnCounterClockwiseEvent.clear()
+            self.turnClockwiseEvent.clear()
+            self.angleOkEvent.set()
+
+
+    def __controlDistance(self, points, distance):
+        # if side length is smaller than expected ---> move forward
+        if points[1][0] - points[0][0] < self.SIDE_SIZES[distance] - self.MARGINS[distance]:
+            self.moveBackwardEvent.clear()
+            self.moveForwardEvent.set()
+        # if side length greater than expected ---> move backwards
+        elif points[1][0] - points[0][0] > self.SIDE_SIZES[distance] + self.MARGINS[distance]:
+            self.moveForwardEvent.clear()
+            self.moveBackwardEvent.set()
+        # if side length within margin of error ---> distance is correct
+        else:
+            self.moveForwardEvent.clear()
+            self.moveBackwardEvent.clear()
+            self.distanceOkEvent.set()
+
+
+    def __controlVerticalPosition(self, frame, points, distance, position):
+        if position == "Under":
+            # if the top side of the QR code is above threshold --> move down
+            if points[0][1] < frame.shape[0] / 2 + self.SIDE_SIZES[distance]:
+                self.moveUpEvent.clear()
+                self.moveDownEvent.set()
+            # if the top side of the QR code is below threshold --> vertical position is correct
+            else:
+                self.moveUpEvent.clear()
+                self.moveDownEvent.clear()
+                self.verticalOkEvent.set()
+        elif position == "Center":
+            # if the top side of the QR code is above threshold --> move down
+            if points[0][1] < frame.shape[0] / 2 - self.SIDE_SIZES[distance] / 2 - self.MARGINS[distance]:
+                self.moveUpEvent.clear()
+                self.moveDownEvent.set()
+            # if the top side of the QR code is below threshold --> move up
+            elif points[0][1] > frame.shape[0] / 2 - self.SIDE_SIZES[distance] / 2 + self.MARGINS[distance]:
+                self.moveDownEvent.clear()
+                self.moveUpEvent.set()
+            # if the top side of the QR code is in between the thresholds --> vertical position is correct
+            else:
+                self.moveDownEvent.clear()
+                self.moveUpEvent.clear()
+                self.verticalOkEvent.set()
+        else:
+            # if the bottom side of the QR code is below threshold --> move up
+            if points[3][1] > frame.shape[0] / 2 - self.SIDE_SIZES[distance]:
+                self.moveDownEvent.clear()
+                self.moveUpEvent.set()
+            # if the bottom side of the QR code is above threshold --> vertical position is correct
+            else:
+                self.moveUpEvent.clear()
+                self.moveDownEvent.clear()
+                self.verticalOkEvent.set()
+
+
+    def __controlHorizontalPosition(self, frame, points, distance, position):
+        if position == "Left":
+            # if the right side of the QR code is right from the threshold --> move left
+            if points[1][0] > frame.shape[1] / 2 - 2 * self.SIDE_SIZES[distance] + self.MARGINS[distance]:
+                self.moveRightEvent.clear()
+                self.moveLeftEvent.set()
+            # if the right side of the Qr code is left from threshold --> horizontal position is correct
+            else:
+                self.moveRightEvent.clear()
+                self.moveLeftEvent.clear()
+                self.horizontalOkEvent.set()
+        elif position == "Center":
+            # if the right side of the QR code is right from the threshold --> move left
+            if points[1][0] > frame.shape[1] / 2 + self.SIDE_SIZES[distance] / 2 + self.MARGINS[distance]:
+                self.moveRightEvent.clear()
+                self.moveLeftEvent.set()
+            # if the right side of the QR code is left from the threshold --> move right
+            elif points[1][0] < frame.shape[1] / 2 + self.SIDE_SIZES[distance] / 2 - self.MARGINS[distance]:
+                self.moveLeftEvent.clear()
+                self.moveRightEvent.set()
+            # if the right side of the Qr code is in between the thresholds --> horizontal position is correct
+            else:
+                self.moveRightEvent.clear()
+                self.moveLeftEvent.clear()
+                self.horizontalOkEvent.set()
+        else:
+            # if the left side of the Qr code is left from threshold --> move right
+            if points[0][0] < frame.shape[1] / 2 - 2 * self.SIDE_SIZES[distance] - self.MARGINS[distance]:
+                self.moveLeftEvent.clear()
+                self.moveRightEvent.set()
+            # if the left side of the Qr code is right from threshold --> horizontal position is correct
+            else:
+                self.moveRightEvent.clear()
+                self.moveLeftEvent.clear()
+                self.horizontalOkEvent.set()
